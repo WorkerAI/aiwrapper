0a687bc73bae51eac53b53cd403b040b5747da2d
Dmitry Kury <dima.kurilchenko@gmail.com>
2023-10-12 03:02:38 +0530

    wip ollama

--- src/lang/lang.ts
+++ src/lang/lang.ts
@@ -1,5 +1,6 @@
 import { OpenAILang, OpenAILangOptions } from "./openai/openai-lang.ts";
 import { AnthropicLang, AnthropicLangOptions  } from "./anthropic/anthropic-lang.ts";
+import { OllamaLang, OllamaLangOptions } from "./ollama/ollama-lang.ts";
 
 /**
  * Lang is a factory class for creating language models from different providers. 
@@ -11,5 +12,9 @@ export abstract class Lang {
 
   static anthropic(options: AnthropicLangOptions): AnthropicLang {
     return new AnthropicLang(options);
+  }
+
+  static ollama(options: OllamaLangOptions): OllamaLang {
+    return new OllamaLang(options);
   }
 }
---
+++ src/lang/ollama/ollama-lang.ts
@@ -0,0 +1,91 @@
+import { LangModelNames } from "../../info.ts";
+import { LangResultWithString, LanguageModel } from "../language-model.ts";
+import { DecisionOnNotOkResponse, httpRequestWithRetry as fetch } from "../../http-request.ts";
+import { processResponseStream } from "../../process-response-stream.ts";
+
+export type OllamaLangOptions = {
+  model?: string;
+  systemPrompt?: string;
+  customCalcCost?: (inTokens: number, outTokens: number) => string;
+};
+
+export type OllamaLangConfig = {
+  name: string;
+  systemPrompt: string;
+  calcCost: (inTokens: number, outTokens: number) => string;
+};
+
+export class OllamaLang extends LanguageModel {
+  _config: OllamaLangConfig;
+
+  constructor(options: OllamaLangOptions) {
+    const modelName = options.model || "mistral";
+    super(modelName);
+    this._config = {
+      name: modelName,
+      systemPrompt: options.systemPrompt || `You are a helpful assistant.`,
+      calcCost: options.customCalcCost || this.defaultCalcCost,
+    };
+  }
+
+  async ask(
+    prompt: string,
+    onResult?: (result: LangResultWithString) => void,
+  ): Promise<LangResultWithString> {
+    const tokensInSystemPrompt =
+    this.tokenizer.encode(this._config.systemPrompt).length;
+    const tokensInPrompt = this.tokenizer.encode(prompt).length;
+
+    const result = new LangResultWithString(prompt, tokensInSystemPrompt + tokensInPrompt);
+
+    const onData = (data: any) => {
+      if (data.finished) {
+        result.finished = true;
+        onResult?.(result);
+        return;
+      }
+
+      if (data.response !== undefined) {
+        const deltaContent = data.response 
+          ? data.response
+          : "";
+
+        result.answer += deltaContent;
+        result.totalTokens = tokensInSystemPrompt + tokensInPrompt +
+          this.tokenizer.encode(result.answer as string).length;
+        // We do it from the config because users may want to set their own price calculation function.
+        result.totalCost = this._config.calcCost(
+          tokensInSystemPrompt + tokensInPrompt,
+          this.tokenizer.encode(result.answer as string).length,
+        );
+
+        onResult?.(result);
+      }
+    };
+
+    console.log("Request");
+
+    console.log(JSON.stringify({
+      model: this._config.name,
+      prompt,
+    }));
+
+    const response = await fetch("http://localhost:11434/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: this._config.name,
+        prompt,
+      }),
+    })
+      .catch((err) => {
+        throw new Error(err);
+      });
+
+    await processResponseStream(response, onData);
+
+    return result;
+  }
+}
