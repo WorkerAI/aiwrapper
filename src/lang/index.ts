42191c3216fccf71b469754437889f63870ca356
Dmitry Kury <dima.kurilchenko@gmail.com>
2024-12-17 16:03:54 +0700

    Export LangResultWithMessages type

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,11 +1,12 @@
 import { Lang } from "./lang.ts";
 import { LangVecs } from "./lang-vecs.ts";
 import { PromptForObject } from "./prompt-for-json.ts";
-import { LanguageModel } from "./language-model.ts";
+import { LanguageModel, LangResultWithMessages } from "./language-model.ts";
 
 export {
   Lang,
   LangVecs,
   LanguageModel,
+  LangResultWithMessages,
   type PromptForObject,
 };
